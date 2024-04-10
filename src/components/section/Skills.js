f9be2522a51da0769e21b109b2ed2538d222707e
LabHasar <brlabhasar1@gmail.com>
2024-04-10 17:36:55 -0300

    section skills

--- src/components/section/Skills.js
+++ src/components/section/Skills.js
@@ -1,9 +1,25 @@
+import sx from "./Skills.module.css";
+import html from "../../images/html.svg";
+import js from "../../images/js.svg";
+import css from "../../images/css.svg";
+import ts from "../../images/ts.svg";
+
 function Skills() {
   return (
-    <div id="skills">
-      <h1>Aqui é o pSkillis</h1>
+    <div id="skills" className={sx.skills}>
+      <h1>
+        <strong>Habilidades</strong>
+      </h1>
+      <p>Conheça um pouco das minhas principais habilidades e conhecimentos.</p>
+      <ul>
+        <li>
+          <img src={html} />
+          <img src={css} />
+          <img src={js} />
+          <img src={ts} />
+        </li>
+      </ul>
     </div>
   );
 }
-
 export default Skills;
