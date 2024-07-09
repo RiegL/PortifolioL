23ccffc2cf6172ab84f8755b3f42752bcdf2e970
RiegL <leoorieg@gmail.com>
2024-07-08 22:15:45 -0300

    fixed footer

--- src/components/section/Footer.js
+++ src/components/section/Footer.js
@@ -16,12 +16,12 @@ function Footer() {
 
   return (
     <div className={sx.footer}>
+      <p>leoorieg@gmail.com</p>
       <ul>
         <li><a href='https://www.instagram.com/leorieg?igsh=MW0yMDNvejRqdnMyMQ%3D%3D&utm_source=qr' target='_blank' rel='noopener noreferrer'><FaInstagram size={30} /></a></li>
         <li><a href='https://github.com/RiegL' target='_blank' rel='noopener noreferrer'><FaGithub size={30} /></a></li>
         <li><a href='https://www.linkedin.com/in/l%C3%A9o-rieg-58b64514a/' target='_blank' rel='noopener noreferrer'><FaLinkedin size={30} /></a></li>
       </ul>
-      <p>leoorieg@gmail.com</p>
       <p>Léo Rieg © {currentTime.format('YYYY')} {currentTime.format('HH:mm:ss')}</p>
     </div>
   );
