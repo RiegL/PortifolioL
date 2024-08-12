8adae7ee6818270b2a4946497d8ca5dc201d13f1
LabHasar <brlabhasar1@gmail.com>
2024-08-12 15:48:44 -0300

    add tsparticles

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React, { useEffect, useState, useMemo } from 'react';
+import Particles, { initParticlesEngine } from '@tsparticles/react';
+import { loadSlim } from '@tsparticles/slim';
 import './App.css';
 import Navbar from './components/section/Navbar';
 import Presentation from './components/section/Presentation';
@@ -6,13 +9,63 @@ import Projects from './components/section/Projects';
 import Footer from './components/section/Footer';
 
 function App() {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+  
+
+  const options = useMemo(() => ({
+    background: {
+      color: { value: "#0d47a1" },
+    },
+    fpsLimit: 120,
+    interactivity: {
+      events: {
+        onClick: { enable: true, mode: "push" },
+        onHover: { enable: true, mode: "repulse" },
+      },
+      modes: {
+        push: { quantity: 1 },
+        repulse: { distance: 200, duration: 0.4 },
+      },
+    },
+    particles: {
+      color: { value: "#f5f5f5" },
+      links: {
+        color: "#f5f5f5",
+        distance:150,
+        enable: true,
+        opacity: 0.5,
+        width: 1,
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outModes: { default: "bounce" },
+        speed: 3,
+      },
+      number: { value: 80 },
+      opacity: { value: 0.4 },
+      shape: { type: "circle" },
+      size: { value: { min: 1, max: 5 } },
+    },
+    detectRetina: true,
+  }), []);
+
   return (
     <div className="App">
-      <Navbar/>
-      <Presentation/>
-      <Skills/>
-      <Projects/>
-      <Footer/>
+      {init && <Particles id="tsparticles" options={options}   />}
+      <Navbar />
+      <Presentation />
+      <Skills />
+      <Projects />
+      <Footer />
     </div>
   );
 }
